43031ef452e30fe1279e25618e41fcd7f88058f5
SrStamm <mirkostamm19@gmail.com>
2026-01-15 18:36:18 +0000

    feat: verify if collection exists and create if not

--- app/main.py
+++ app/main.py
@@ -1,8 +1,20 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from app.features.extraction.router import router as extraction_router
+from app.features.rag.rag_client import get_rag_client, RAGClient
 from app.features.rag.router import router as rag_router
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("Iniciando APP")
+    print("Verificando colección")
+    rag_client: RAGClient = get_rag_client()
+    rag_client.create_collection()
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(extraction_router)
 app.include_router(rag_router)
