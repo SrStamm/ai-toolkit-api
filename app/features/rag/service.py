1efc239589a0d483997cfa77b07210eba96ec9a8
SrStamm <mirkostamm19@gmail.com>
2026-01-15 19:27:42 +0000

    feat: chunk_text and ingest_document added

--- app/features/rag/service.py
+++ app/features/rag/service.py
@@ -2,11 +2,13 @@ from fastapi import Depends
 from app.core.llm_client import LLMClient, get_llm_client
 import requests
 from bs4 import BeautifulSoup
+from app.features.rag.rag_client import RAGClient, get_rag_client
 
 
 class RAGService:
-    def __init__(self, llm_client: LLMClient):
+    def __init__(self, llm_client: LLMClient, rag_client: RAGClient):
         self.llm_client = llm_client
+        self.rag_client = rag_client
 
     def _clean_text(self, text: str) -> str:
         lines = [line.strip() for line in text.splitlines()]
@@ -31,8 +33,34 @@ class RAGService:
 
         return self._clean_text(text)
 
+    def chunk_text(self, text: str, max_chars=300, overlap=100):
+        chunks = []
+        start = 0
+
+        while start < len(text):
+            end = start + max_chars
+            chunk = text[start:end]
+            chunks.append(chunk)
+            start = end - overlap
+
+        return chunks
+
+    def ingest_document(self, text, source, domain, topic):
+        chunks = self.chunk_text(text)
+
+        for chunk in chunks:
+            payload = {
+                "text": chunk,
+                "source": source,
+                "domain": domain,
+                "topic": topic,
+            }
+
+            self.rag_client.insert_vector(chunk, payload)
+
 
 def get_rag_service(
     llm_client: LLMClient = Depends(get_llm_client),
+    rag_client: RAGClient = Depends(get_rag_client),
 ):
-    return RAGService(llm_client)
+    return RAGService(llm_client, rag_client)
