2da4bd0bb41f317e351b01ca056fafb14429c58d
SrStamm <mirkostamm19@gmail.com>
2026-01-25 14:01:29 +0000

    feat: vector interface created

---
+++ app/features/rag/interfaces.py
@@ -0,0 +1,28 @@
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from typing import Any, List
+
+
+@dataclass
+class FilterContext:
+    domain: str
+    topic: str
+
+
+class VectorStoreInterface(ABC):
+    @abstractmethod
+    def query(
+        self, query_vector: List[float], limit: int, filter_context: FilterContext
+    ) -> list[Any]:
+        """Search similar vectors"""
+        pass
+
+    @abstractmethod
+    def insert_vector(self, points: List[Any]) -> None:
+        """Insert or update vectors"""
+        pass
+
+    @abstractmethod
+    def rerank(self, query: str, search_result: list) -> List[Any]:
+        """Sort order results"""
+        pass
