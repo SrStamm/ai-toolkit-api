d3568ea31651b670fdc63f123d70e49c17a03dab
SrStamm <mirkostamm19@gmail.com>
2026-02-07 18:22:06 +0000

    feat: redis and celery basic config

---
+++ app/core/celery_app.py
@@ -0,0 +1,12 @@
+import os
+from celery import Celery
+
+redis_url = os.getenv("REDIS_URL")
+
+celery_app = Celery(
+    "ai_toolkit",
+    broker=redis_url,
+    # backend=settings.CELERY_RESULT_BACKEND,
+)
+
+celery_app.autodiscover_tasks(["app.features.rag.jobs"])
