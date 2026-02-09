ca7c54b6319c3e0c9cd4c54ad76d0a1fe2dee4f1
SrStamm <mirkostamm19@gmail.com>
2026-02-09 10:54:21 +0000

    feat: ingest_files_job added

--- app/features/rag/router.py
+++ app/features/rag/router.py
@@ -1,10 +1,12 @@
 import asyncio
 import json
+import shutil
+from pathlib import Path
 from fastapi import APIRouter, Depends, File, Form, Request, UploadFile
 from fastapi.responses import StreamingResponse
 import structlog
 
-from .jobs.celery_tasks import ingest_html_job
+from .jobs.celery_tasks import ingest_file_job, ingest_html_job
 from .jobs.job_service import JobService
 from ..extraction.exceptions import EmptySourceContentError
 from ..rag.exceptions import ChunkingError, EmbeddingError, error_event
@@ -179,6 +181,37 @@ async def ingest_document_job(
     ingest_html_job.delay(job_id, ingest.model_dump())
 
     return {"status": "queued", "url": ingest.url, "job_id": job_id}
+
+@router.post(
+    "/ingest-file/job",
+)
+async def ingest_file_job_endpoint(
+    file: UploadFile = File(...),
+    source: str = Form(...),
+    domain: str = Form(...),
+    topic: str = Form(...),
+    job_serv: JobService = Depends(JobService)
+):
+    if not file.filename.lower().endswith(".pdf"):
+        return {"status": "error", "message": "File must be a PDF"}
+
+
+    # create job_id
+    job_id = job_serv.create()
+
+    # Define route in shared volume
+    upload_path = Path('/backend/api_data') / f"{job_id}.pdf"
+    upload_path.parent.mkdir(parents=True, exist_ok=True)
+
+    # Save file
+    with upload_path.open("wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    # create task
+    ingest_file_job.delay(job_id, str(upload_path), file.filename, domain, topic)
+
+    # return status and job_id
+    return {"status": "queued", "job_id": job_id}
 
 
 @router.get(
