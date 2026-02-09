43177f0830dc25130ed9e1fecc6aee9e772418a0
SrStamm <mirkostamm19@gmail.com>
2026-02-09 14:06:52 +0000

    feat: in mobile, ingestinterface float

--- front-ai-toolkit/src/pages/Home.tsx
+++ front-ai-toolkit/src/pages/Home.tsx
@@ -3,9 +3,15 @@ import IngestionInterface from "@/components/ingestionInterface";
 
 function Home() {
   return (
-    <main className="flex flex-row md:flex-row min-h-screen bg-background">
-      <section className="md:w-1/3 border-r">
+    <main className="relative flex md:static md:flex-row min-h-screen bg-background">
+      <section className="hidden md:block md:relative md:w-1/3 md:border-r">
         <IngestionInterface />
+      </section>
+
+      <section className="absolute md:hidden">
+        <Sheet>
+          <IngestionInterface />
+        </Sheet>
       </section>
 
       <section className="md:w-2/3">
