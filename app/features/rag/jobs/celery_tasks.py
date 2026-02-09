e15b9c33e4cd6a8c7653f3f08a42ba8584d04da8
SrStamm <mirkostamm19@gmail.com>
2026-02-09 10:52:46 +0000

    feat: created ingest_file_job

--- app/features/rag/jobs/celery_tasks.py
+++ app/features/rag/jobs/celery_tasks.py
@@ -1,7 +1,9 @@
 # celery tasks - wrapper
 
 import asyncio
+import os
 
+from fastapi import UploadFile
 import structlog
 
 from .schemas import JobStatus
@@ -48,3 +50,50 @@ def ingest_html_job(self, job_id: str, ingest_data: dict):
         logger.error("ingest_job_failed", job_id=job_id, error=str(e), exc_info=True)
         job_service.fail(job_id, str(e))
         raise
+
+
+
+@celery_app.task(bind=True)
+def ingest_file_job(self, job_id: str, file_path: str, source, domain: str, topic: str):
+    job_service = JobService()
+    rag_service: RAGService = get_rag_service()
+
+    logger.info("ingest_job_started", job_id=job_id, file_path=file_path)
+
+    try:
+        job_service.update_status(job_id, JobStatus.running)
+
+        async def tracker(percent, message):
+            logger.info(
+                "ingest_job_progress", job_id=job_id, progress=percent, step=message
+            )
+            job_service.update_progress(job_id, percent, message)
+
+        with open(file_path, "rb") as f:
+            fake_upload_file = UploadFile(
+                file=f, 
+                filename=os.path.basename(file_path)
+            )
+
+            asyncio.run(
+                rag_service.ingest_pdf_file(
+                    file=fake_upload_file,
+                    source=source,
+                    domain=domain,
+                    topic=topic,
+                    progress_callback=tracker
+                )
+            )
+
+
+        logger.info("ingest_job_success", job_id=job_id)
+
+        job_service.update_progress(job_id, 100)
+        job_service.update_status(job_id, JobStatus.completed)
+
+    except Exception as e:
+        logger.error("ingest_job_failed", job_id=job_id, error=str(e), exc_info=True)
+        job_service.fail(job_id, str(e))
+    finally:
+        if os.path.exists(file_path):
+            os.remove(file_path)
