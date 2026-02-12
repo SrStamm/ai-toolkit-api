d5171739394d4373c352435ba511b8461d75da94
SrStamm <mirkostamm19@gmail.com>
2026-02-12 09:25:50 +0000

    fix: script start or create news containers

--- scripts/start_dev_monitoring.sh
+++ scripts/start_dev_monitoring.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-docker compose --profile monitoring start && docker compose logs -f
+docker compose --profile monitoring start || docker compose --profile monitoring up --detach
+
+docker compose logs -f
 
 docker compose --profile monitoring stop
