2b5603ffb8c793fc4b0cf38ff8c8f3e740ea60c5
SrStamm <mirkostamm19@gmail.com>
2026-03-04 17:59:17 +0000

    feat: query added

--- app/api/llamaindex/orchrestator.py
+++ app/api/llamaindex/orchrestator.py
@@ -1,5 +1,9 @@
+from llama_index.core import VectorStoreIndex
 from .ingestion import LlamaIngester
 from .indexing import LlamaIndexer
+from .config import setup_llamaindex
+
+setup_llamaindex()
 
 class LlamaIndexOrchestrator:
     def __init__(self):
@@ -10,10 +14,19 @@ class LlamaIndexOrchestrator:
         storage_context = self.indexer.get_storage_context()
         return self.ingester.ingest_pdf(pdf_path, storage_context)
 
+    def query(self, query: str):
+        index = VectorStoreIndex.from_vector_store(
+            vector_store=self.indexer.vectore_store,
+        )
+
+        query_engine = index.as_query_engine()
+
+        return query_engine.query(query)
+
 
 if __name__ == "__main__":
     orchrestator = LlamaIndexOrchestrator()
-    result = orchrestator.proccess_pdf("api/llamaindex/data/AI Engineering.pdf")
+    # result = orchrestator.proccess_pdf("api/llamaindex/data/AI Engineering.pdf")
+    result2 = orchrestator.query("¿Qué es la ingeniería de IA?")
 
-    print("PDF indexado correctamente")
-    print(result)
+    print(result2)
