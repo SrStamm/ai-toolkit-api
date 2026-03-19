3e6ae5f3dabc65d4bd6e98df54ca96a5257d91cf
SrStamm <mirkostamm19@gmail.com>
2026-03-19 10:26:35 +0000

    feat: added RagTool

--- app/api/agent/model.py
+++ app/api/agent/model.py
@@ -1,11 +1,29 @@
 from abc import ABC, abstractmethod
+from ..llamaindex.orchrestator import (
+    LlamaIndexOrchestrator,
+    get_orchestrator,
+)
 
 class Tool(ABC):
     name: str
     description: str
 
     @abstractmethod
-    def execute(self, input: str) -> str:
+    def execute(self, input: str):
         pass
 
+TOOLS = {}
 
+class RagTool(Tool):
+    name = "rag"
+    description = "Busqueda en base vectorial"
+
+    def __init__(self, rag: LlamaIndexOrchestrator):
+        self.rag = rag
+
+    def execute(self, input: str):
+        return self.rag.custom_query(query=input)
+
+def rag_instance() -> RagTool:
+    rag=get_orchestrator()
+    return RagTool(rag)
