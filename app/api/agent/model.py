16390f214c5089f054bb9a993b175d6ea0681e70
SrStamm <mirkostamm19@gmail.com>
2026-03-19 10:35:38 +0000

    feat: created ToolResponse and updated RagTool

--- app/api/agent/model.py
+++ app/api/agent/model.py
@@ -1,18 +1,22 @@
 from abc import ABC, abstractmethod
+from typing import Any
+from pydantic import BaseModel
 from ..llamaindex.orchrestator import (
     LlamaIndexOrchestrator,
     get_orchestrator,
 )
+
+class ToolResponse(BaseModel):
+    output: str
+    metadata: dict[str, Any] = {}
 
 class Tool(ABC):
     name: str
     description: str
 
     @abstractmethod
-    def execute(self, input: str):
+    def execute(self, input: str) -> ToolResponse:
         pass
-
-TOOLS = {}
 
 class RagTool(Tool):
     name = "rag"
@@ -21,9 +25,21 @@ class RagTool(Tool):
     def __init__(self, rag: LlamaIndexOrchestrator):
         self.rag = rag
 
-    def execute(self, input: str):
-        return self.rag.custom_query(query=input)
+    def execute(self, input: str) -> ToolResponse:
+        res = self.rag.custom_query(query=input)
+        return ToolResponse(
+            output=res.answer,
+            metadata={
+                "citations": res.citations,
+                "metadata": res.metadata
+            }
+        )
 
 def rag_instance() -> RagTool:
     rag=get_orchestrator()
     return RagTool(rag)
+
+def build_tool_registry():
+    return {
+        "rag": rag_instance(),
+    }
